fdb8cc31da99676c09eb093228d068737838bf6f
Kulikov Gleb <kulgleb15@gmail.com>
2026-06-09 21:06:21 +0300

    feat: Базовый роутинг с заглушками компонентов

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'countries',
+    pathMatch: 'full',
+  },
+  {
+    path: 'countries',
+    loadComponent: () =>
+      import('./features/countries/countries').then((m) => m.Countries),
+  },
+  {
+    path: 'cities',
+    loadComponent: () =>
+      import('./features/cities/cities').then((m) => m.Cities),
+  },
+];
---
+++ src/app/features/cities/cities.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cities',
+  imports: [],
+  templateUrl: './cities.html',
+  styleUrl: './cities.css',
+})
+export class Cities {}
