a4e2cca8d8d4a94c96c3eb6ae0e285db74cd4bd4
Kulikov Gleb <kulgleb15@gmail.com>
2026-06-10 18:59:38 +0300

    feat: Добавлено getCities

--- src/app/core/services/geo.service.ts
+++ src/app/core/services/geo.service.ts
@@ -3,22 +3,43 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApiResponse } from '../models/api-response.model';
 import { Country } from '../models/country.model';
+import { City } from '../models/city.model';
 
 
 @Injectable({ providedIn: 'root' })
 export class GeoService {
-  private http = inject(HttpClient);  //
+  private http = inject(HttpClient); //
   private baseUrl = 'http://geodb-free-service.wirefreethought.com/v1/geo';
 
-  getCountries(limit : number, offset : number, namePrefix?:string) : Observable<ApiResponse<Country>> {
-    let params = new HttpParams().set('limit', limit).set('offset', offset );
+  getCountries(
+    limit: number,
+    offset: number,
+    namePrefix?: string,
+  ): Observable<ApiResponse<Country>> {
+    let params = new HttpParams().set('limit', limit).set('offset', offset);
 
-    if(namePrefix){
+    if (namePrefix) {
       params = params.set('namePrefix', namePrefix);
     }
 
     return this.http.get<ApiResponse<Country>>(`${this.baseUrl}/countries`, { params });
   }
 
-  // getCities
+  getCities(
+    limit: number,
+    offset: number,
+    countryIds?: string,
+    namePrefix?: string,
+  ): Observable<ApiResponse<City>> {
+    let params = new HttpParams().set('limit', limit).set('offset', offset);
+
+    if (countryIds) {
+      params = params.set('countryIds', countryIds);
+    }
+    if (namePrefix) {
+      params = params.set('namePrefix', namePrefix);
+    }
+
+    return this.http.get<ApiResponse<City>>(`${this.baseUrl}/cities`, { params });
+  };
 }
