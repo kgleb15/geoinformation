a8c3336d570e391212681bb52e0dce64be9aad75
Kulikov Gleb <kulgleb15@gmail.com>
2026-06-13 12:56:02 +0300

    style: Улучшен внешний вид header
    + заглушки под смену языков

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatRadioGroup, FormsModule, MatRadioButton],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  selectedLang = 'ru';
+}
